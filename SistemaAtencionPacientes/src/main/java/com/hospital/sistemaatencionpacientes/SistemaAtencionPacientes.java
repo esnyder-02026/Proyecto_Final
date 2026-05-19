e507d3d563b6a048d2db9503c6403423709ea48a
Usuario <Usuario@LAPTOP-1CEQ3EQG>
2026-05-18 21:49:25 -0600

    Configuracion del pom.xml

---
+++ SistemaAtencionPacientes/src/main/java/com/hospital/sistemaatencionpacientes/SistemaAtencionPacientes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hospital.sistemaatencionpacientes;
+
+/**
+ *
+ * @author Usuario
+ */
+public class SistemaAtencionPacientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
